364c17f46b600013fc3e9f1daf9c51cce7e6ea02
gggsssaaa <89472093+gggsssaaa@users.noreply.github.com>
2024-05-07 15:24:20 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import { generate } from "generate-nice-eth-address";
+
+function generateAddress777(postfix) {
+    let result = generate("777");
+    return result;
+}
+
+export { generateAddress777 };
